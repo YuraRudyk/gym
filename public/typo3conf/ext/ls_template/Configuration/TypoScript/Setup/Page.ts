c3a65afa8b4797b57174495dce0c8a870060654b
Yurii Rudyk <yuriy.rudyk@711media.de>
2018-12-16 11:24:44 +0200

    add bootsrap

--- public/typo3conf/ext/ls_template/Configuration/TypoScript/Setup/Page.ts
+++ public/typo3conf/ext/ls_template/Configuration/TypoScript/Setup/Page.ts
@@ -21,23 +21,32 @@ page {
     }
 
     headerData {
-        10 = TEXT
-        10.value (
-            <script>
-                window._tiConfig = window._tiConfig || {
-                    tiDomain: 'responder.wt-safetag.com',
-                    tiId: '427096606843090',
-                    // customDomain: 'sub.domain.tld',
-                    // customPath: 'js/container_script.js',
-                    option: {}
-                };
-
-                /** start TagIntegration loader  */
-                (function(d,c,a,f){d.wts=d.wts||[];var g=function(b){var a="";b.customDomain&&b.customPath?a=b.customDomain+"/"+b.customPath:b.tiDomain&&b.tiId&&(a=b.tiDomain+"/resp/api/get/"+b.tiId+"?url="+encodeURIComponent(d.location.href)+"&v=5");if(b.option)for(var c in b.option)a+="&"+c+"="+encodeURIComponent(b.option[c]);return a};if(-1===c.cookie.indexOf("wt_r=1")){var e=c.getElementsByTagName(a)[0];a=c.createElement(a);a.async=!0;a.onload=function(){if("undefined"!==typeof d.wt_r&&!isNaN(d.wt_r)){var b=
-                new Date,a=b.getTime()+1E3*parseInt(d.wt_r);b.setTime(a);c.cookie="wt_r=1;path=/;expires="+b.toUTCString()}};a.src="//"+g(f);e.parentNode.insertBefore(a,e)}})(window,document,"script",_tiConfig);
-                /** end TagIntegration loader */
-            </script>
+        4 = TEXT
+        4.value (
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         )
+
+
+#        10 = TEXT
+#        10.value (
+#            <script>
+#                window._tiConfig = window._tiConfig || {
+#                    tiDomain: 'responder.wt-safetag.com',
+#                    tiId: '427096606843090',
+#                    // customDomain: 'sub.domain.tld',
+#                    // customPath: 'js/container_script.js',
+#                    option: {}
+#                };
+#
+#                /** start TagIntegration loader  */
+#                (function(d,c,a,f){d.wts=d.wts||[];var g=function(b){var a="";b.customDomain&&b.customPath?a=b.customDomain+"/"+b.customPath:b.tiDomain&&b.tiId&&(a=b.tiDomain+"/resp/api/get/"+b.tiId+"?url="+encodeURIComponent(d.location.href)+"&v=5");if(b.option)for(var c in b.option)a+="&"+c+"="+encodeURIComponent(b.option[c]);return a};if(-1===c.cookie.indexOf("wt_r=1")){var e=c.getElementsByTagName(a)[0];a=c.createElement(a);a.async=!0;a.onload=function(){if("undefined"!==typeof d.wt_r&&!isNaN(d.wt_r)){var b=
+#                new Date,a=b.getTime()+1E3*parseInt(d.wt_r);b.setTime(a);c.cookie="wt_r=1;path=/;expires="+b.toUTCString()}};a.src="//"+g(f);e.parentNode.insertBefore(a,e)}})(window,document,"script",_tiConfig);
+#                /** end TagIntegration loader */
+#            </script>
+#        )
     }
 
     10 = FLUIDTEMPLATE
